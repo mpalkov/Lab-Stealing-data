8fe0e2638bbb117040b4c71578690473c66cfdb7
Martin Palkov <martin.palkov@gmail.com>
2023-11-07 17:19:58 +0100

    refactor code

--- seed.js
+++ seed.js
@@ -3,25 +3,23 @@ const Character = require("./Character.model");
 const { log } = require("console");
 require('dotenv').config();
 
-let cleanedData = null;
-
-fetch("https://rickandmortyapi.com/api/character")
-    .then((data) => data.json())
+mongoose.connect(process.env.MONGODB_URI)
+    .then(()=> {
+        console.log("connected to DB")
+        return fetch("https://rickandmortyapi.com/api/character");
+    })
+    .then(data => data.json())
     // jsonData.results[]
-    .then((jsonData) => {
+    .then(jsonData => {
        // console.log(jsonData)
-        cleanedData = jsonData.results.map((element) => {
+        const cleanedData = jsonData.results.map((element) => {
             return ({name: element.name, imageUrl: element.image});
         });
-        //console.log(cleanedData);
-        return mongoose.connect(process.env.MONGODB_URI) // add the name of the database at the end
+        return cleanedData;
     })
-    .then(()=> {
-        console.log("connected to DB");
-        return Character.insertMany(cleanedData);
-    })
-    .then(()=> {
-        console.log("Created all the documents in the DB\nDisconnecting");
+    .then(cleanedData => Character.insertMany(cleanedData))
+    .then(() => {
+        console.log("Created all the documents in the DB\nDisconnecting...");
         return mongoose.disconnect();
     })
-    .catch((error) => console.log(error));
+    .catch(error => console.log(error));
